a85286c647985297af3fab74fae048eece149c8c
Vanshika <vanshika.tyagi1147@gmail.com>
2026-09-11 11:46:29 +0530

    usecase-2

---
+++ usecase2.java
@@ -0,0 +1,46 @@
+abstract class Shape {
+    abstract double area();
+
+    void displayArea() {
+        System.out.println("Area = " + area());
+    }
+}
+
+class Circle extends Shape {
+    double radius;
+
+    Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    double area() {
+        return Math.PI * radius * radius;
+    }
+}
+
+class Rectangle extends Shape {
+    double length;
+    double width;
+
+    Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    double area() {
+        return length * width;
+    }
+}
+public class usecase2 {
+    public static void main(String[] args) {
+        Shape[] shapes = new Shape[2];
+        shapes[0] = new Circle(5.0);
+        shapes[1] = new Rectangle(4.0, 6.0);
+ 
+        for (Shape s : shapes) {
+            s.displayArea();   // must print correct area for each shape
+        }
+    }
+}
